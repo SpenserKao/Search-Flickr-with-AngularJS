fe48c20bba5a4a03288a54b54bebaabc4ad82f84
Spenser Kao <SpenserKao@Optusnet.com.au>
2017-12-28 12:44:11 +1100

    revised.

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 var appFlickrAPI = angular.module('formFlickr', []);
-appFlickrAPI.controller('FlickrController', ['$scope', '$http', function ($scope, $http) {
-	$scope.showSelectedImage = false;
+appFlickrAPI.controller('FlickrController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+// appFlickrAPI.controller('FlickrController', ['$scope', '$http', function ($scope, $http) {	
+	// $scope.showSelectedImage = false;
+	$http.get("releasenote.json").success(function(data) {
+		$scope.notes = data.releasenote;
+	});
+
+	$http.get("todos.json").success(function(data) {
+		$scope.todos = data.todos;
+	});
+	
+	$scope.commonArea = 'main';
 	
 	$scope.master = {};
 	$scope.images = {};
 
+	$scope.openNewTab = function(selectedImageURL) {
+	$scope.url = selectedImageURL;
+
+	// $scope.redirectToGoogle = function(){
+		// // $window.open(selectedImageURL, '_blank');
+		// $scope.url = selectedImageURL;
+	// };	
+	document.getElementById("link2image").style.color = "red";
+	window.open(selectedImageURL, '_blank');
+	// return true;
+	};
+
+	// $("#link2Image").click(function(e){
+		// e.preventDefault();//this will prevent the link trying to navigate to another page
+		// var href = $(this).attr("href");//get the href so we can navigate later
+		// $window.location = href;
+	// });
+
+	// $("#link2Image").on('click', function(event) {
+		// event.preventDefault();
+		// window.location = "http://www.google.com";
+	// });
+	 
 	$scope.search = function (searchCriteria) {
 	if (searchCriteria.tags === undefined || searchCriteria.tags.trim() === "") {
 	  searchCriteria.tags = null;
@@ -46,16 +79,22 @@ appFlickrAPI.controller('FlickrController', ['$scope', '$http', function ($scope
 		$scope.form.tags.$setValidity();
 		$scope.images = {};
 		$scope.searchCriteria = {};
-		$scope.showSelectedImage = false;
+		// $scope.showSelectedImage = false;
+		$scope.commonArea='main';
 	};
 
 	// select an image when click on one
 	$scope.clickAnImage = function (i) {
-		 $scope.showSelectedImage = true;
+		 // $scope.showSelectedImage = true;
 		 $scope.title = i.title;
 		 $scope.author = i.author;
 		 $scope.tags = i.tags;
 		 $scope.image = i.link;
+		 $scope.commonArea = 'selectedImage';
 	};
 		
+	// click commonArea: releaseNote or toDo list
+	$scope.selectCommonArea = function (c) {
+		$scope.commonArea = c;
+	}
 }]);
